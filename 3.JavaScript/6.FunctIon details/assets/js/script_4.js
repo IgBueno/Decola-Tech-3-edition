0734034bf45c7c1782ece275f20767ef015636ea
igBueno <igordsb402@gmail.com>
2022-06-23 21:36:25 -0300

    Adding my new caculator

--- 3.JavaScript/6.FunctIon details/assets/js/script_4.js
+++ 3.JavaScript/6.FunctIon details/assets/js/script_4.js
@@ -1,7 +1,21 @@
 // Tipos de função
 
 // declarativas
-function funcao (){
-  console.log('Mensagem de uma função declarativa')
-}
-funcao();
+// function funcao (){
+//   console.log('Mensagem de uma função declarativa')
+// }
+// funcao(); 
+
+//  Expressão de função
+//  com nomeação
+// var funcao = function funcao() {
+//   console.log('Sou uma mensagem de função de expressão')
+// }
+
+// funcao();
+
+// Arrow function
+// var funcao = () => {
+//   console.log('Sou uma arrow function!')
+// }
+// funcao()
